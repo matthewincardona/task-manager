c2c6a4ee3719ffe483b9c6ff73b178b1f53b7759
Matthew Incardona <matthewincardona@imac.wireless.rit.edu>
2024-02-11 20:01:04 -0500

    Add local storage... hopefully

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,9 +1,29 @@
+import 'dart:convert';
+
 class Todo {
   final String title;
   bool isCompleted;
 
   Todo({
     required this.title,
-    this.isCompleted = false, // Default value is false
+    this.isCompleted = false,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  String toJsonString() {
+    return json.encode(toJson());
+  }
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      title: json['title'],
+      isCompleted: json['isCompleted'] ?? false,
+    );
+  }
 }
--- lib/view_model/todo_view_model.dart
+++ lib/view_model/todo_view_model.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../model/todo.dart';
+import 'dart:convert';
 
 class TodoViewModel extends ChangeNotifier {
   final List<Todo> _todos = [];
@@ -12,33 +14,38 @@ class TodoViewModel extends ChangeNotifier {
   // Method to add a todo
   void addTodo(String title) {
     _todos.add(Todo(title: title));
+    _saveTodos();
     notifyListeners();
   }
 
   // Method to toggle a todo
   void toggleTodo(int index) {
     _todos[index].isCompleted = !_todos[index].isCompleted;
+    _saveTodos();
     notifyListeners();
   }
 
   // Method to clear all todos
   void clearTodos() {
     _todos.clear();
+    _clearStoredTodos();
     notifyListeners();
   }
 
   // Method to calculate the time remaining until midnight EST
   String calculateTimeUntilMidnight() {
-    Duration timeUntilMidnight = DateTime(
-      DateTime.now().year,
-      DateTime.now().month,
-      DateTime.now().day + 1,
-      0,
-      0,
-    ).difference(DateTime.now());
+    DateTime now = DateTime.now();
+    DateTime midnight = DateTime(now.year, now.month, now.day + 1, 0, 0);
+    _timeUntilMidnight = midnight.difference(now);
 
-    int hours = timeUntilMidnight.inHours;
-    int minutes = timeUntilMidnight.inMinutes.remainder(60);
+    if (_timeUntilMidnight.inSeconds <= 0) {
+      // If countdown is zero or negative, clear todos and return empty string
+      clearTodos();
+      return '';
+    }
+
+    int hours = _timeUntilMidnight.inHours;
+    int minutes = _timeUntilMidnight.inMinutes.remainder(60);
 
     return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}';
   }
@@ -46,19 +53,47 @@ class TodoViewModel extends ChangeNotifier {
   // Method to start the countdown timer
   void startCountdownTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_timeUntilMidnight.inSeconds > 0) {
-        _timeUntilMidnight -= const Duration(seconds: 1);
-        notifyListeners();
-      } else {
+      String timeRemaining = calculateTimeUntilMidnight();
+      if (timeRemaining.isEmpty) {
+        // If countdown is empty, cancel timer
         _timer.cancel();
+      } else {
+        notifyListeners();
       }
     });
   }
 
+  // Method to save todos to storage
+  Future<void> _saveTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> todoStrings =
+        _todos.map((todo) => todo.toJsonString()).toList();
+    await prefs.setStringList('todos', todoStrings);
+  }
+
+  // Method to load todos from storage
+  Future<void> loadTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? todoStrings = prefs.getStringList('todos');
+    if (todoStrings != null) {
+      _todos.clear();
+      _todos.addAll(todoStrings
+          .map((todoString) => Todo.fromJson(json.decode(todoString))));
+    }
+  }
+
+  // Method to clear stored todos from storage
+  Future<void> _clearStoredTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('todos');
+  }
+
   // Constructor to set up the countdown timer when the view model is created
   TodoViewModel() {
-    calculateTimeUntilMidnight();
-    startCountdownTimer();
+    loadTodos().then((_) {
+      calculateTimeUntilMidnight();
+      startCountdownTimer();
+    });
   }
 
   // Dispose method to cancel the timer when the view model is no longer needed
